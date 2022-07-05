de77ecc3b104bf7589f928e09667da7551b949b4
Szymon Swiatczynski <szymswiat.ml@gmail.com>
2022-07-05 22:53:37 +0200

    Add pyodide to project

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,41 @@
 // Modified from the create-react-app TypeScript template
 // See: https://github.com/facebook/create-react-app/blob/main/packages/cra-template-typescript/template/src/App.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './app.css';
 
+// @ts-ignore
+import { loadPackage } from 'pyodide/load-package';
+// @ts-ignore
+import { loadPyodide } from 'pyodide/pyodide';
+// @ts-ignore
+import * as pyodide from 'pyodide/api';
+
 import logo from './icons/logo.svg';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="app" role="main">
-        <header className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
-          <p>
-            Edit <code>src/app.tsx</code> and save to reload.
-          </p>
-          <a
-            className="app-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App: React.FC = () => {
+  useEffect(() => {
+    loadPyodide();
+  });
+
+  return (
+    <div className="app" role="main">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/app.tsx</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
